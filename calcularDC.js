ef6b4da995ca86420f4d6525a09228909c8f3664
Sergi Almacellas Abellana <sergi@koolpi.com>
2013-12-31 10:07:02 +0100

    Add function to calculate iban based on a Spain account number

--- calcularDC.js
+++ calcularDC.js
@@ -25,3 +25,47 @@ var entidad = "0509";
 var cuenta = "0100050958";
 
 console.log(calcularDC(banco,entidad,cuenta));
+
+// Modulo 97 for huge numbers given as digit strings.
+function mod97(digit_string)
+{
+  var m = 0;
+  for (var i = 0; i < digit_string.length; ++i)
+    m = (m * 10 + parseInt(digit_string.charAt(i))) % 97;
+  return m;
+}
+
+function capital2digits(ch)
+{
+  var capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (var i = 0; i < capitals.length; ++i)
+    if (ch == capitals.charAt(i))
+      break;
+  return i + 10;
+}
+
+function calcIBANforSpain(banco, entidad, dc, cuenta){
+    //Adapted from http://toms-cafe.de/iban/iban.js
+  var code     = 'ES';
+  var bban     = banco.trim() + entidad.trim() + dc.trim() + cuenta.trim()
+
+  // Assemble digit string
+  var digits = "";
+  for (var i = 0; i < bban.length; ++i)
+  {
+    var ch = bban.charAt(i).toUpperCase();
+    if ("0" <= ch && ch <= "9")
+      digits += ch;
+    else
+      digits += capital2digits(ch);
+  }
+  for (var i = 0; i < code.length; ++i)
+  {
+    var ch = code.charAt(i);
+    digits += capital2digits(ch);
+  }
+
+  checksum = 98 - mod97(digits);
+  return code + checksum + bban;
+}
+
